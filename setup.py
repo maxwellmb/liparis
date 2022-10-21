983738ee9604e8d06feb03b920dc32e928941de0
Maxwell Millar-Blanchaer <maxmb.astro@gmail.com>
2022-10-20 20:02:21 -0700

    setup.py file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='liparis',
+   version='0.1',
+   description='data reduction pipline or the ORKID instrument at Keck',
+   author='Jack Grossman, Max Millar-Blanchaer',
+   author_email='maxmb@ucsb.edu',
+   packages=['liparis'],  # would be the same as name
+)
+
